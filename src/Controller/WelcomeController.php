9509f63b3e88cce1e7703eb71df804bbcef46bdc
HectornsGit <hector.novoa.segade@gmail.com>
2023-11-09 20:19:14 +0100

    feat [WelcomeController] adds an endpoint to check server availability

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+#Basic controller to check if the application is live.
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HealthController extends AbstractController
+{
+    #[Route(path:"/welcome", name:"welcome", methods:["GET"])]
+    public function healthCheck(Request $request):Response
+    {
+        $name= $request->query->get("name","Héctor");
+        return new Response("Welcome, ". $name. "!",Response::HTTP_ACCEPTED);
+    }
+}
