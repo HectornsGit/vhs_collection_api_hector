fea779cbf9d015ae50b458c37ab19136b32dd903
HectornsGit <hector.novoa.segade@gmail.com>
2023-11-12 20:57:05 +0100

    Refactor WelcomeController changes the route to "/" and the default message to be more generic

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class WelcomeController extends AbstractController
 {
-    #[Route(path: "/welcome", name: "welcome", methods: ["GET"])]
+    #[Route(path: "/", name: "welcome", methods: ["GET"])]
     public function welcoming(Request $request): Response
     {
-        $name = $request->query->get("name", "Héctor");
-        return new Response("Welcome, " . $name . "!", Response::HTTP_OK);
+        $name = $request->query->get("name", "Earth inhabitant");
+        return new JsonResponse(["message" => "Welcome, " . $name . "!", "status" => Response::HTTP_OK], Response::HTTP_OK);
     }
 }
