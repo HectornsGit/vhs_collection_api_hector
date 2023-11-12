c4929fd63f683748ac2cddc14afc4d6fa98a32b3
HectornsGit <hector.novoa.segade@gmail.com>
2023-11-12 20:50:02 +0100

    Feat [Vhs,VhsRepository] adds vhs entity

---
+++ src/Entity/Vhs.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VhsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VhsRepository::class)]
+class Vhs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $collection_name = null;
+
+    #[ORM\Column]
+    private ?int $moviedb_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCollectionName(): ?string
+    {
+        return $this->collection_name;
+    }
+
+    public function setCollectionName(string $collection_name): static
+    {
+        $this->collection_name = $collection_name;
+
+        return $this;
+    }
+
+    public function getMoviedbId(): ?int
+    {
+        return $this->moviedb_id;
+    }
+
+    public function setMoviedbId(int $moviedb_id): static
+    {
+        $this->moviedb_id = $moviedb_id;
+
+        return $this;
+    }
+}
