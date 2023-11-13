a65addb06cf7e097635ebac2bc1490558d7561db
HectornsGit <hector.novoa.segade@gmail.com>
2023-11-13 19:42:03 +0100

    Feat [fetchMovieFromMovieDbById,AddVhsToCollectionController] adds a function to fetch a movie from moviedb

---
+++ src/utils/fetchMovieFromMovieDbById.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+function fetchMovieFromMovieDbById(int $moviedb_id, HttpClientInterface $httpClient): stdClass|string
+{
+
+    $apiToken = $_ENV["API_TOKEN"];
+    $url = "https://api.themoviedb.org/3/movie/" . $moviedb_id;
+
+    try {
+
+        $response = $httpClient->request("GET", $url, [
+            "headers" => [
+                "Authorization" => "Bearer " . $apiToken,
+                "accept" => "application/json",
+            ],
+        ]);
+
+        $data = json_decode($response->getContent());
+
+        return $data;
+    } catch (\Exception $e) {
+        return ['error' => $e->getMessage()];
+    }
+}
